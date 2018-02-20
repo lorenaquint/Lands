15af2cce18b78c54a156973b8a58ae99f3107f54
lorenaquint <lorenaquint@gmail.com>
2018-02-20 15:41:55 -0500

    Agragar model View Model

---
+++ Lands/Lands/Infraestructure/InstanceLocator.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace Lands.Infraestructure
+{
+    using ViewModels;
+  
+    public class InstanceLocator
+    {
+        #region propiedades
+        public MainViewModel Main { get; set; }
+        #endregion
+        #region Constructor
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+        #endregion
+    }
+}
