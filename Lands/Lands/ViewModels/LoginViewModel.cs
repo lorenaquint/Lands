d11874c354122278c30f6d0328c92abe270eb519
lorenaquint <lorenaquint@gmail.com>
2018-02-26 23:44:06 -0500

    LoginViewModel

---
+++ Lands/Lands/ViewModels/LoginViewModel.cs
@@ -0,0 +1,46 @@
+﻿
+
+
+namespace Lands.ViewModels
+{
+    using System.Windows.Input;
+    public class LoginViewModel
+    {
+        #region Propiedades
+        public string  Email
+        {
+            get;
+            set;
+        }
+        public string Password
+        {
+            get;
+            set;
+        }
+        public bool IsRunning
+        {
+            get;
+            set;
+        }
+        public bool IsRemembered
+        {
+            get;
+            set;
+        }
+        #endregion
+        #region Comandos
+          public ICommand LoginCommand
+        {
+            get;
+            set;
+        }
+        #endregion
+        #region Constructores
+        public LoginViewModel()
+        {
+            IsRemembered = true;
+        }
+        #endregion
+       
+    }
+}
--- Lands/Lands/ViewModels/MainViewModel.cs
+++ Lands/Lands/ViewModels/MainViewModel.cs
@@ -1,10 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿
 
 namespace Lands.ViewModels
 {
     public class MainViewModel
     {
+        #region ViewModel
+        public LoginViewModel Login
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Constructores
+        public MainViewModel()
+        {
+            this.Login = new LoginViewModel();
+        }
+        #endregion
     }
 }
