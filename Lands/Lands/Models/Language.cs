1ed3f834f77d9af9e1dc0e3b7b8469c6bddf3ee9
lorenaquint <lorenaquint@gmail.com>
2018-03-04 11:02:58 -0500

    Servicios

---
+++ Lands/Lands/Models/Language.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace Lands.Models
+{
+    using Newtonsoft.Json;
+    public class Language
+    {
+
+
+        [JsonProperty(PropertyName = "iso6391")]
+        public string Iso639_1 { get; set; }
+        [JsonProperty(PropertyName = "iso6392")]
+        public string Iso639_2 { get; set; }
+        [JsonProperty(PropertyName = "Name")]
+        public string Name { get; set; }
+        [JsonProperty(PropertyName = "NativeName")]
+        public string NativeName { get; set; }
+    }
+
+}
---
+++ Lands/Lands/Models/Translations.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Lands.Models
+{
+    using Newtonsoft.Json;
+    public class Translations
+    {
+        [JsonProperty(PropertyName = "de")]
+        public string Gernamy { get; set; }
+        [JsonProperty(PropertyName = "sp")]
+        public string Spanish { get; set; }
+        [JsonProperty(PropertyName = "fr")]
+        public string France { get; set; }
+        [JsonProperty(PropertyName = "ja")]
+        public string Japanese { get; set; }
+        [JsonProperty(PropertyName = "it")]
+        public string Italian { get; set; }
+        [JsonProperty(PropertyName = "br")]
+        public string Brazilian { get; set; }
+        [JsonProperty(PropertyName = "pt")]
+        public string Portuges { get; set; }
+        [JsonProperty(PropertyName = "nl")]
+        public string Dutch { get; set; }
+        [JsonProperty(PropertyName = "hr")]
+        public string Croatian { get; set; }
+        [JsonProperty(PropertyName = "fa")]
+        public string Danish { get; set; }
+    }
+}
